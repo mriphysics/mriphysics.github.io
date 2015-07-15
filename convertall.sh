ec22ea178ee4717ea02968d4459217c093951f82
Shaihan Malik <shaihan.malik@gmail.com>
2015-07-15 14:00:47 +0100

    Added convert.sh -> this updates all md files into html including the correct title tags

---
+++ convertall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./md2strapdownhtml.sh dsc.md 'Direct Signal Control'
+./md2strapdownhtml.sh index.md 'mriphysics.github.io'
+./md2strapdownhtml.sh rfpulsedes.md 'rf pulse design'
+./md2strapdownhtml.sh emsims.md 'electromagnetic simulations'
