9b20bf2e843e2b0b27ceed8a8c061a509f7cde7a
Shaihan Malik <shaihan.malik@gmail.com>
2016-10-10 22:14:19 +0100

    updated teaching materials Oct 2016

--- convertall.sh
+++ convertall.sh
@@ -5,7 +5,7 @@
 ./md2strapdownhtml.sh emsims.md 'electromagnetic simulations'
 ./md2strapdownhtml.sh fieldmapping.md 'fieldmapping'
 ./md2strapdownhtml.sh teaching.md 'teaching'
-./md2strapdownhtml.sh teaching-mri-intro.md 'Masters: medical imaging sciences'
+./md2strapdownhtml_MJ.sh teaching-mri-intro.md 'MRES: Medical Imaging Sciences'
 ./md2strapdownhtml.sh teaching-rf-pulses.md 'Masters: advanced physics of MRI'
 ./md2strapdownhtml.sh teaching-sequence-sims.md 'Masters: advanced physics of MRI'
 ./md2strapdownhtml.sh teaching-antennas.md 'BEng: electrical engineering II'
---
+++ md2strapdownhtml_MJ.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mdfile="$1"
+file=${mdfile%.*}
+# echo $file
+htmlfile="$file.html"
+# echo $htmlfile
+> $htmlfile
+printf "<!DOCTYPE html>\n<html>\n<head><script type=""text/javascript"" async \n src=""https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML"">\n</script>\n</head><title>$2</title>\n<xmp theme=""spacelab"" style=""display:none;"">""\n" >> $htmlfile
+cat $mdfile >> $htmlfile
+printf "</xmp>\n<script src=""http://strapdownjs.com/v/0.2/strapdown.js""></script>\n</html>" >> $htmlfile
